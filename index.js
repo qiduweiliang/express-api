a64704c269417b495e2e5e5b61e389d197ddcd3f
qiduweiliang <920049929@qq.com>
2016-09-29 16:14:30 +0800

    fist

---
+++ index.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/api')
+
+
+
+var db = mongoose.connection
+
+// db.on('error', console.error.bind(console, 'connection error:'))
+
+db.once('open', function() {
+  var userSchema = mongoose.Schema({
+    name: String,
+    password: String,
+    age: String
+  })
+
+  var User = mongoose.model('user', userSchema);
+  // cat 是实际数据库中记录的名字
+  // var qidu = new user({ name: 'qidu', password: '111', age: '24' });
+  // // 成功构建一条数据记录
+  // qidu.save()
+  //
+  // qidu.name = 'qiduweiliang'
+  // qidu.password = 'aaabbb'
+  // qidu.save()
+
+  User.findById({_id: '57ecb8c5b080a90d628a7a4c'}, function(err, user) {
+    user.name = 'rrrrrr'
+    user.save(function(err){
+      console.log('更新了！')
+      User.find().exec(function(err, users) {
+        // 异步执行
+        console.log(users);
+      });
+    });
+  });
+  console.log("我先出来了")
+
+  // console.log(User.find());
+})
